408991d1376590ff5066834945a4b02c82729554
yash <yashnarela01@gmail.com>
2024-10-28 14:40:21 +0530

    second commit

--- script.js
+++ script.js
@@ -3,16 +3,9 @@ let nav = document.querySelector(".btn");
 nav.addEventListener("click", () => {
   document.body.classList.toggle("active");
 });
-
-let target = document.querySelector(".timezone");
-
+let now = new Date();
+let day = now.getDay();
 function updateDateTime() {
-  let now = new Date();
-  const formattedDate = now.toLocaleDateString(); // Format as MM/DD/YYYY or according to locale
-  const formattedTime = now.toLocaleTimeString();
-
-  let day = now.getDay();
-
   let arr = [
     "sunday",
     "monday",
@@ -25,8 +18,30 @@ function updateDateTime() {
 
   console.log(arr[day]);
 
+  const formattedDate = now.toLocaleDateString(); // Format as MM/DD/YYYY or according to locale
+  const formattedTime = now.toLocaleTimeString();
+  let target = document.querySelector(".timezone");
+
+  let hours = now.getHours();
+  console.log(hours);
+
+  let minutes = now.getMinutes();
+
+  let seconds = now.getSeconds();
+
+  let containerbodyminute = document.querySelector(".containerbodyminute");
+
+  let containerbodyhours = document.querySelector(".containerbodyhours");
+
+  let containerbodysecond = document.querySelector(".containerbodysecond");
+
+  containerbodyhours.innerText = hours;
+  containerbodyminute.innerText = minutes;
+
+  containerbodysecond.innerText = seconds;
+
   target.innerText = `${arr[day]}  ${formattedDate} ${formattedTime}`;
 }
 setInterval(() => {
-  updateDateTime(), 1000;
-});
+  updateDateTime();
+}, 1000);
